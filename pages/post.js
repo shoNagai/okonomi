db9d8f3777c99605e32c38490a55ab4c011c8614
sho <nagaishou@Sho.local>
2018-07-22 08:55:41 +0900

    fix layout & save ipfs

--- pages/post.js
+++ pages/post.js
@@ -1,12 +1,28 @@
 import { Component } from 'react'
-import PostContentList from '../components/postContentList'
+import { initStore } from '../mobx/store'
+import PostContentBox from '../components/postContentBox'
 
 export default class Post extends Component {
+  static getInitialProps ({ req }) {
+    const isServer = !!req
+    const store = initStore(isServer)
+    return { isServer }
+  }
+
+  constructor (props) {
+    super(props)
+    this.store = initStore(props.isServer)
+  }
+
   render() {
     return(
-      <div>
-        <PostContentList />
-      </div>
+      <section className="hero page">
+        <div className="hero-body">
+          <div className="container">
+            <PostContentBox store={this.store} />
+          </div>
+        </div>
+      </section>
     )
   }
 }
