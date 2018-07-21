d71de62e741e410767b54d42d569a3725e6e65f3
sho <nagaishou@Sho.local>
2018-07-21 14:22:43 +0900

    add router & page

---
+++ pages/post.js
@@ -0,0 +1,5 @@
+export default () => (
+  <div>
+    <p>This is the Post</p>
+  </div>
+)
--- utils/uport.js
+++ utils/uport.js
@@ -1,7 +1,7 @@
 import { Connect, SimpleSigner } from 'uport-connect'
 
 export const uport = new Connect('cuplist', {
-  clientId: process.env.UPORT_CLIENT_ID,
-  network: process.env.UPORT_NETWORK,
-  signer: SimpleSigner(process.env.UPORT_SIGNING_KEY)
+  clientId: "2omQi6HGhS1j7ZGg3i1JP2Rmb7ssbXqXPnr",
+  network: "rinkeby",
+  signer: SimpleSigner("87276ca61fa7b7742414de67cf0e013690d6230e0bf66bd2f09e6280b608dbfe")
 })
