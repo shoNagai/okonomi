3297193148aafbacfa3b47e650cd1dbc0a0793df
Yuka Tsuboi <yuka.tsuboi@trustdock.io>
2018-07-21 14:16:57 +0900

    move login action to store

--- mobx/store.js
+++ mobx/store.js
@@ -1,5 +1,18 @@
-import { observable } from 'mobx'
+import { observable, action } from 'mobx'
+import { uport } from '../utils/uport'
 
 export default class Store {
   @observable currentUser = undefined
+
+  @action
+  login() {
+    uport.requestCredentials({
+      requested: ['name'],
+      notification: true
+    })
+    .then(credential => {
+      console.log(credential)
+      this.currentUser = credential
+    })
+  }
 }
--- pages/index.js
+++ pages/index.js
@@ -1,21 +1,10 @@
 import { Component } from 'react'
 import Store from '../mobx/store'
-import { uport } from '../utils/uport'
 
 export default class Index extends Component {
   constructor(props) {
     super(props)
     this.store = new Store
-  }
-
-  login() {
-    uport.requestCredentials({
-      requested: ['name'],
-      notification: true
-    })
-    .then(credential => {
-      console.log(credential)
-    })
   }
 
   render() {
@@ -24,7 +13,7 @@ export default class Index extends Component {
       <section className="hero is-middle">
         <div className="hero-body">
           <div className="container">
-            <button className="button is-primary" onClick={this.login}>
+            <button className="button is-primary" onClick={this.store.login}>
               Login
             </button>
           </div>
