617baf2fcfc47a6cd69d891c5efd5862c7eb7fb4
Ryoga Miki <r.y.mz0601@gmail.com>
2018-07-21 19:02:57 +0900

    addPost addLike addDislike 追加

--- truffle/contracts/Okonomi.sol
+++ truffle/contracts/Okonomi.sol
@@ -1,7 +1,11 @@
 pragma solidity ^0.4.23;
-
+pragma experimental ABIEncoderV2;
+import "github.com/OpenZeppelin/zeppelin-solidity/contracts/math/SafeMath.sol";
 
 contract Okonomi {
+    
+    using SafeMath for uint;
+    
     struct Post {
         uint postId;
         uint like;
@@ -16,8 +20,8 @@ contract Okonomi {
     struct Step {
         uint postId;
         uint stepId;
-        uint comment;
-        uint photo;
+        string comment;
+        string photo;
     }
 
     Step[] public steps;
@@ -30,17 +34,43 @@ contract Okonomi {
         posts.push(Post(0, 0, 0, "溜池山王", "銀座線", "南北線")); 
     }
 
-    function serch(string _station, string _toLine, string _fromLine) public returns (Post) {
-        for (uint i = 0; p < posts.length; i++) {
-            if (_station == posts[i].station
-                && _fromLine == posts[i].fromLine
-                && _toLine == posts[i].toLine) {
+    function search(string _station, string _fromLine, string _toLine) public returns (Post) {
+        for (uint i = 0; i < posts.length; i++) {
+            if (keccak256(_station) == keccak256(posts[i].station)
+                && keccak256(_fromLine) == keccak256(posts[i].fromLine)
+                && keccak256(_toLine) == keccak256(posts[i].toLine)) {
                 
                 return posts[i];
 
             }
         }
     }
+    
+    function addPost(string[] _comments, string[] _photos, string _station, string _fromLine, string _toLine) public {
+
+        require(_comments.length == _photos.length);
+        for (uint i = 0; i < _comments.length; i++) {
+            steps.push(Step(posts.length, i, _comments[i], _photos[i]));
+        }
+        
+        posts.push(Post(posts.length, 0, 0, _station, _fromLine, _toLine)); 
+    }
+    
+    function addLike(uint _postId) public  returns (Post) {
+        require(posts.length > _postId);
+        
+        posts[_postId].like = posts[_postId].like.add(1);
+        return posts[_postId];
+    }
+    
+    function addDislike(uint _postId) public  returns (Post) {
+        require(posts.length > _postId);
+        
+        posts[_postId].dislike = posts[_postId].dislike.add(1);
+        return posts[_postId];
+    }
+    
+    
 
 
 }
