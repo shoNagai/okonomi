85c9b8e75555fa06098a841b7bbc16aa9f75c081
ookimaki <ookimaki@givinglog.com>
2018-07-22 10:33:22 +0900

    ookimaki

--- utils/settings.js
+++
@@ -1,4 +0,0 @@
-export default {
-    // ethAddressDefault: "0x33A464CdA2CA86710b6d37F20DB92192E4595DCE",
-    erc20Address: "0xe523efe0afc9703f6bcb530a07917262878ac5ba"
-  }
--- utils/uport.js
+++ utils/uport.js
@@ -1,7 +1,20 @@
 import { Connect, SimpleSigner } from 'uport-connect'
+import contractABI from './contractABI.json'
+var mnid = require('web3')
 
 export const uport = new Connect('cuplist', {
-  clientId: process.env.UPORT_CLIENT_ID,
-  network: process.env.UPORT_NETWORK,
-  signer: SimpleSigner(process.env.UPORT_SIGNING_KEY)
+  clientId: '2oepTPEPXguAmSftVKp5qq98E58ydDUnkfL',
+  network: 'rinkeby',
+  signer: SimpleSigner('523bb16bcdb713d26b83e67dc48108333467dd771cb07784a79d6bdd89f7a343')
 })
+
+
+export const web3 = uport.getWeb3()
+
+//This is for ro
+export const myContractABI = uport.contract(contractABI)
+export const myContract = myContractABI.at("0xe523efe0afc9703f6bcb530a07917262878ac5ba")
+
+//This is for call
+export const myContractABI2 = web3.eth.contract(contractABI)
+export const myContract2 = myContractABI2.at("0xe523efe0afc9703f6bcb530a07917262878ac5ba")
